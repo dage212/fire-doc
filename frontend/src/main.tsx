3a4b4d5eea25fb507ebbbb921f3610368342f905
zhaohaifeng <m18235447245@163.com>
2025-07-19 19:34:37 +0800

    feat:  目录问题，更新后backend文件夹下载不到本地

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,7 +10,7 @@ const RootContainer = styled.div`
   width: 100%;
   height: 100%;
 `
-
+console.log(123123)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RootContainer>
